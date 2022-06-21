ef63a2a16b1d7a2c28f7d2b6afdaf8d91da0ec6f
ribamarf01 <ribamar.f01@gmail.com>
2022-06-21 15:22:53 -0300

    header mobile view

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react'
 import Link from 'next/link'
 
 const Header = () => {
+
+  const [activeMenu, setActiveMenu] = useState(false)
+
+  const changeMenuState = () => {
+    setActiveMenu(!activeMenu)
+  }
 
   return (<>
     <header className="w-full h-16 bg-rich-black-fogra flex items-center justify-between fixed top-0 z-10">
@@ -10,13 +17,21 @@ const Header = () => {
           <p className="text-3xl font-bold text-white font-poppins -tracking-widest group-hover:text-rhythm duration-300 transition-all">Know your price</p>
         </div>
       </Link>
-      <ul className='flex items-center mr-8'>
-        <li className='px-3 text-white hover:text-rhythm duration-300 transition-all cursor-pointer font-medium font-poppins text-lg tracking-tighter'>Inicio</li>
-        <li className='px-3 text-white hover:text-rhythm duration-300 transition-all cursor-pointer font-medium font-poppins text-lg tracking-tighter'>Objetivo</li>
-        <li className='px-3 text-white hover:text-rhythm duration-300 transition-all cursor-pointer font-medium font-poppins text-lg tracking-tighter'>Planos</li>
-        <li className='px-3 text-white hover:text-rhythm duration-300 transition-all cursor-pointer font-medium font-poppins text-lg tracking-tighter'>Suporte</li>
+      { activeMenu ?  
+        <svg className="w-6 h-6 text-white mr-8 md:hidden cursor-pointer" onClick={()=>changeMenuState()} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /></svg>
+        :
+        <svg className="w-6 h-6 text-white mr-8 md:hidden cursor-pointer" onClick={()=>changeMenuState()} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
+      }
+      
+      <ul className={`md:flex md:items-center mr-5 ${activeMenu ? 'absolute' : 'hidden'} md:top-0 top-16 bg-rich-black-fogra w-full md:w-auto text-center`}>
+        <li className='px-3 py-2 md:py-0 text-white hover:text-rhythm duration-300 transition-all cursor-pointer font-medium font-poppins text-2xl md:text-lg tracking-tighter'>Inicio</li>
+        <li className='px-3 py-2 md:py-0 text-white hover:text-rhythm duration-300 transition-all cursor-pointer font-medium font-poppins text-2xl md:text-lg tracking-tighter'>Objetivo</li>
+        <li className='px-3 py-2 md:py-0 text-white hover:text-rhythm duration-300 transition-all cursor-pointer font-medium font-poppins text-2xl md:text-lg tracking-tighter'>Planos</li>
+        <li className='px-3 py-2 md:py-0 text-white hover:text-rhythm duration-300 transition-all cursor-pointer font-medium font-poppins text-2xl md:text-lg tracking-tighter'>Suporte</li>
 
-        <li className='px-3 ml-3 py-1 rounded text-white bg-rhythm hover:bg-independence hover:text-alice-blue duration-300 transition-all cursor-pointer font-medium text-lg font-poppins tracking-tighter'>Calculadora</li>
+        <li className='md:py-0 mx-3 mb-3 md:mb-0 flex justify-center'>
+          <p className='px-3 py-2 md:w-full w-1/3 rounded text-white bg-rhythm hover:bg-independence hover:text-alice-blue duration-300 transition-all cursor-pointer font-medium text-2xl md:text-lg font-poppins tracking-tighter'>Calculadora</p>
+        </li>
       </ul>
     </header>
   </>)
