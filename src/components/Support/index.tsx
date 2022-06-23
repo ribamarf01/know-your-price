9761558fb5ce3d6b8b46ebebae4bb041c3720f0a
ribamarf01 <ribamar.f01@gmail.com>
2022-06-23 20:21:53 -0300

    Toast for support message

--- src/components/Support/index.tsx
+++ src/components/Support/index.tsx
@@ -1,4 +1,7 @@
 import { FormEvent, useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 import { send } from '@emailjs/browser'
 
 interface Form {
@@ -10,9 +13,9 @@ interface Form {
 
 const EMPTY_FORM: Form = {
   name: '',
-  email: '', 
-  subject: '', 
-  content: '', 
+  email: '',
+  subject: '',
+  content: '',
 }
 
 const Support = () => {
@@ -30,15 +33,30 @@ const Support = () => {
     }, 'L7v95KUvrXilSDXLi')
       .then(res => {
         setForm(EMPTY_FORM)
-        console.log(res.text)
+        toast.success("Sua mensagem foi enviada com sucesso! 😎", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        })
       })
       .catch(err => {
-        setForm(EMPTY_FORM)
-        console.log(err.text)
+        toast.warn("Houve um erro 😰. Você pode tentar novamente?", {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        })
       })
   }
 
-  return(<>
+  return (<>
     <div>
       <div className="flex flex-col items-center w-full bg-rich-black-fogra py-4">
         <h1 className="text-5xl font-poppins text-alice-blue mx-4 -tracking-wider font-bold">Suporte</h1>
@@ -49,16 +67,16 @@ const Support = () => {
 
         <form className='flex flex-col items-center w-full mx-4 md:w-1/3' onSubmit={sendSupportMessage}>
           <label className='text-xl font-bold py-2 text-alice-blue' htmlFor="name">Nome:</label>
-          <input className='text-alice-blue text-lg bg-transparent border-maya-blue border rounded outline-none text-center p-2 w-2/3' placeholder='Seu nome' id='name' type="text" value={form.name} onChange={e => setForm({ ...form, name: e.target.value })}/>
+          <input className='text-alice-blue text-lg bg-transparent border-maya-blue border rounded outline-none text-center p-2 w-2/3' placeholder='Seu nome' id='name' type="text" value={form.name} onChange={e => setForm({ ...form, name: e.target.value })} />
 
           <label className='text-xl font-bold py-2 text-alice-blue' htmlFor="email">E-mail para contato:</label>
-          <input className='text-alice-blue text-lg bg-transparent border-maya-blue border rounded outline-none text-center p-2 w-2/3' placeholder='Email para contato' id='email' type="text" value={form.email} onChange={e => setForm({ ...form, email: e.target.value })}/>
+          <input className='text-alice-blue text-lg bg-transparent border-maya-blue border rounded outline-none text-center p-2 w-2/3' placeholder='Email para contato' id='email' type="text" value={form.email} onChange={e => setForm({ ...form, email: e.target.value })} />
 
           <label className='text-xl font-bold py-2 text-alice-blue' htmlFor="subject">Motivo:</label>
-          <input className='text-alice-blue text-lg bg-transparent border-maya-blue border rounded outline-none text-center p-2 w-2/3' placeholder='Motivo' id='subject' type="text" value={form.subject} onChange={e => setForm({ ...form, subject: e.target.value })}/>
+          <input className='text-alice-blue text-lg bg-transparent border-maya-blue border rounded outline-none text-center p-2 w-2/3' placeholder='Motivo' id='subject' type="text" value={form.subject} onChange={e => setForm({ ...form, subject: e.target.value })} />
 
           <label className='text-xl font-bold py-2 text-alice-blue' htmlFor="content">Descrição:</label>
-          <textarea className='text-alice-blue text-lg bg-transparent border-maya-blue border rounded outline-none text-center p-2 w-2/3 resize-none h-36' placeholder='Descrição' id='content' value={form.content} onChange={e => setForm({ ...form, content: e.target.value })}/>
+          <textarea className='text-alice-blue text-lg bg-transparent border-maya-blue border rounded outline-none text-center p-2 w-2/3 resize-none h-36' placeholder='Descrição' id='content' value={form.content} onChange={e => setForm({ ...form, content: e.target.value })} />
 
           <div className="py-2"></div>
 
@@ -68,8 +86,10 @@ const Support = () => {
 
         <div className="py-4"></div>
 
+        <ToastContainer />
+
       </div>
-    </div>  
+    </div>
   </>)
 
 }
