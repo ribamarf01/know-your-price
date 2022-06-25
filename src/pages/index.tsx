b8602179b4024f50e698a170e8628c38a4043cac
ribamarf01 <ribamar.f01@gmail.com>
2022-06-25 19:59:31 -0300

    footer component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,18 @@ import Head from 'next/head'
 import Hero from '@components/Hero'
 import Pricing from '@components/Pricing'
 import Support from '@components/Support'
+import Footer from '@components/Footer'
 
 const Home = () => {
   return (<>
     <Head>
-      <title>Home</title>
+      <title>Know your price</title>
     </Head>
 
     <Hero />
     <Pricing />
     <Support />
+    <Footer />
   </>)
 }
 
